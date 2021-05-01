9b02c26b10e5e63c2458de3ff7e6d99347a7134d
Iambizi <amirbzmn@gmail.com>
2021-04-30 20:25:07 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/scss/_globals.scss'
+import '../styles/scss/_index.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
