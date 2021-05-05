cb4fd92df34bbe6d8bf6dafe0225e5595992aa19
Iambizi <amirbzmn@gmail.com>
2021-05-05 15:21:07 -0400

    adds canvas

--- components/homeContent/carousel.tsx
+++ components/homeContent/carousel.tsx
@@ -1,4 +1,5 @@
 import styles from "../../styles/scss/homePage/_home.module.scss";
+import WarpedImage from "../../components/homeContent/warpedImage";
 export default function carousel():JSX.Element{
     const videos = [
         "Bryson Tiller 'Always Forever'",
@@ -8,11 +9,15 @@ export default function carousel():JSX.Element{
     return(
         <>
             <h1 className={styles.carouselTitle}>{videos[0]}</h1>
+            {/* <WarpedImage /> */}
             <section className={styles.homeContentSection}>
-                
-                <p className={styles.previousButton}>PREVIOUS</p>
-                <p className={styles.nextButton}>NEXT</p>
+            <WarpedImage />
             </section>
+            <p className={styles.nextButton}>NEXT</p>
+            <p className={styles.previousButton}>PREVIOUS</p>
+            
+            
+            {/* <p className ={styles.counter}></p> */}
         </>
     )
 }
--- components/homeContent/warpedImage.tsx
+++ components/homeContent/warpedImage.tsx
@@ -1,6 +1,9 @@
+import styles from "../../styles/scss/homePage/_home.module.scss";
 export default function warpedImage():JSX.Element{
     return(
         <>
+            <canvas className={styles.scene}>
+            </canvas>
         </>
     )
 }
