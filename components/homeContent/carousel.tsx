77f23e6f667f32ab96a684120f7a3547ab7c4689
Iambizi <amirbzmn@gmail.com>
2021-05-12 22:57:52 -0400

    carousel styling

--- components/homeContent/carousel.tsx
+++ components/homeContent/carousel.tsx
@@ -8,16 +8,18 @@ export default function carousel({count, projects}): JSX.Element{
     return(
         <>
         {/* onChange add classname to add transition changing styles */}
-            <div className={styles.gallery}>
-                <div className={styles.carousel}>
-                        <img
-                            className={styles.carouselImage}
-                            src={"https://kidstudio.co/content/2-home" + `${projects[count].path}`}
-                            alt={"Video Project screenshot"}
-                            height={200}
-                            width={330}
-                        />
-                    </div>
+            <div className={styles.slider}>
+                {projects.map((p,i)=>
+                    <div className={styles.carousel} key={i}>
+                    <img
+                        className={styles.carouselImage}
+                        src={"https://kidstudio.co/content/2-home" + `${projects[count].path}`}
+                        alt={"Video Project screenshot"}
+                        height={200}
+                        width={330}
+                    />
+                </div>
+                )}
             </div>
         </>
     )
