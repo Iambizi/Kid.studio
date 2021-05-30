bbb1bd7a664cf03246d627600086d15c00024c48
Iambizi <amirbzmn@gmail.com>
2021-05-30 00:07:02 -0400

    adds wrapper around carousel content

--- components/homeContent/carousel.tsx
+++ components/homeContent/carousel.tsx
@@ -16,19 +16,19 @@ export default function carousel({ homeProjects, carouselX, titleX  }:Type): JSX
         {/* onChange add classname to add transition changing styles */}
             <div className={styles.slider} style={{left: `${ -carouselX }%`}}>
                 {homeProjects.map((p,i)=>
-                    <div key={i}>
-                        <Link href={"/work/[project]"} key={i}>
-                            <h2 className={`${styles[homeProjects[i].className]}`}>{homeProjects[i].videoTitle}</h2>
-                        </Link>
-                        <div className={`${styles.carousel}`}>
-                            <img
-                                className={styles.carouselImage}
-                                src={"https://kidstudio.co/content/2-home" + `${homeProjects[i].path}`}
-                                alt={"Video Project screenshot"}
-                                height={200}
-                                width={330}
-                            />
-                        </div>
+                    <div className={styles.sliderWrapper} key={i}>
+                            <Link href={"/work/[project]"}>
+                                <h2 className={`${styles[homeProjects[i].className]}`}>{homeProjects[i].videoTitle}</h2>
+                            </Link>
+                            <div className={`${styles.carousel}`} key={homeProjects[i]}>
+                                <img
+                                    className={styles.carouselImage}
+                                    src={"https://kidstudio.co/content/2-home" + `${homeProjects[i].path}`}
+                                    alt={"Video Project screenshot"}
+                                    height={200}
+                                    width={330}
+                                />
+                            </div>
                     </div>
                 )}
             </div>
