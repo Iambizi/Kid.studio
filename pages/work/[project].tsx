3e4f527945819b68201620c2ef5accb8f895946a
Iambizi <amirbzmn@gmail.com>
2021-06-11 12:42:04 -0400

    projectPages: adds data from prop

--- pages/work/[project].tsx
+++ pages/work/[project].tsx
@@ -4,57 +4,65 @@ import { GetStaticPaths, GetStaticProps} from 'next';
 import fs from 'fs'
 import path from 'path'
 
+interface Type{
+    projects: any;
+}
 
-export default function projectPages(){
+export default function projectPages({projects}: Type){
+    console.log(projects);
     return(
         <>
             <Meta page={"Reel"} />
             <Layout>
-                <h1>New video Project</h1>
+                <h1>{projects.title}</h1>
             </Layout>
         </>
     )
 }
 
-// export const getStaticProps: GetStaticProps = async (context)=>{
-//     const  { params } = context;
-//     const base = process.env.NEXT_PUBLIC_APP_DOMAIN
-//     const projectPath = params.path;
-//     const fileToRead = path.join(base,'./projects.json')
-//     const data = JSON.parse(await fs.readFileSync(fileToRead).toString())
-//     const project = data.blogs.find(project => project.id === projectPath)
-//     return {
-//         props: {
-//             work: project
-//         }
-//     }
-// }
+export const getStaticProps: GetStaticProps = async (context)=>{
+    const  { params } = context;
+    const projectPath = params.path;
+    const fileToRead = path.join(process.cwd(),'./projectsData/projects.json');
+    const data = JSON.parse(await fs.readFileSync(fileToRead).toString());
+    // const project = data.projects.find(project => project.path === projectPath)
+    const projects = data.projects.map((item, i)=>(data.projects[i]))
+    // console.log(data.projects[0].path)
+    console.log(projects);
+    return {
+        props: {
+            projects: projects
+        }
+    }
+}
 
 export const getStaticPaths: GetStaticPaths = async () =>{
     return {
-        paths: [{params: {project:'ya'}}],
+        paths: [
+            {params: {projects:'ya'}}
+        ],
         fallback: 'blocking'
     };
 }
 
-export const getStaticProps: GetStaticProps = async ({params})=>{
-    return {
-        props: {
-            work: [
-                {
-                    // title: "BRYSON TILLER 'ALWAYS FOREVER'",
-                    path: "/bryson-tiller-always-forever"
-                },
-                {
-                    // title: "BIG SEAN 'WOLVES' FT. POST MALONE",
-                    path: "/wolves"
-                },
-                {
-                    // title: "MUSTAFA 'AIR FROCES'",
-                    path: "/mustafa-air-forces"
-                }
-            ]
-        }
-    }
-}
+// export const getStaticProps: GetStaticProps = async ({params})=>{
+//     return {
+//         props: {
+//             work: [
+//                 {
+//                     // title: "BRYSON TILLER 'ALWAYS FOREVER'",
+//                     path: "/bryson-tiller-always-forever"
+//                 },
+//                 {
+//                     // title: "BIG SEAN 'WOLVES' FT. POST MALONE",
+//                     path: "/wolves"
+//                 },
+//                 {
+//                     // title: "MUSTAFA 'AIR FROCES'",
+//                     path: "/mustafa-air-forces"
+//                 }
+//             ]
+//         }
+//     }
+// }
 
