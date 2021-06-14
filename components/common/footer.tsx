c0303e1b3153d851b1f5ee5fc9f589e8b8195c43
Iambizi <amirbzmn@gmail.com>
2021-06-14 11:52:46 -0400

    footer: fixes footer links

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -13,11 +13,11 @@ export default function footer({ bgImg }:Type):JSX.Element{
                 <ul className={ bgImg ? `${styles.footerLinks} ${styles.hoverColor}` : `${styles.footerLinks}` }>
                     <a className={styles.footerLink} href={"http://instagram.com/kidstudio_"}>INSTAGRAM</a>
                     /
-                    <Link href={"/work/reel"}>
+                    <Link href={ process.env.NEXT_PUBLIC_APP_DOMAIN + "/work/reel"}>
                         <a className={styles.footerLink}>REEL</a>
                     </Link>
                     /
-                    <a className={styles.footerLink} href={""}>EMAIL@KIDSTUDIO.CO</a>
+                    <a className={styles.footerLink} href={"mailto:email@kidstudio.co"}>EMAIL@KIDSTUDIO.CO</a>
                 </ul>
             </footer>
         </>
