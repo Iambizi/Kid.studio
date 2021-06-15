d29e8284576e41263385e964c189bf4b2c67bab0
Iambizi <amirbzmn@gmail.com>
2021-06-15 15:30:51 -0400

    backEndData: removes caps on project names

--- components/workContent/projectPages/mainInfoSection.tsx
+++ components/workContent/projectPages/mainInfoSection.tsx
@@ -3,13 +3,20 @@ interface Type{
     projects: any;
 }
 export default function mainInfoSection( {projects} ):JSX.Element{
-
+    const projectDetails = [
+        {
+            
+        }
+    ]
     return(
         <>
-            <section className={styles.mainInfo}>
-                <h1>
+            <section className={styles.projectPageSection}>
+                <h3 className={styles.projectTitle}>
                     {projects.title}
-                </h1>
+                </h3>
+                <p className={styles.projectCreds}>
+                    
+                </p>
             </section>
         </>
     )
