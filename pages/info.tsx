20eddd0ee9a2df7b08bd06b8673bda6c4a91b410
Iambizi <amirbzmn@gmail.com>
2021-06-19 15:39:21 -0400

    projectPages: adds max width and height to imgs

--- pages/info.tsx
+++ pages/info.tsx
@@ -1,7 +1,14 @@
 import Layout from '../components/layout';
 import Meta  from '../components/common/meta';
+import React, { useEffect } from "react";
 
 export default function info():JSX.Element{
+// removes needsScroll class set in project pages from vertical scroll
+// projectPage useEffect hook needs refactoring to avoid calling it again here.
+  useEffect(()=>{
+    const bg = document.body;
+    bg.classList.remove("needsScroll");
+},[]);
     return(
         <>
             <Meta page={"Info"} />
