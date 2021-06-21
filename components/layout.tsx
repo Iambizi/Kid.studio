545f30ab29f82a14031774023e5e931dd5d7fec4
Iambizi <amirbzmn@gmail.com>
2021-06-21 09:20:04 -0400

    projectList: css adjustments

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,9 @@
 import Navigation from "../components/common/header/navigation"
 import Footer from "../components/common/footer";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import projectPages from "../pages/work/[project]";
+import styles from "../styles/scss/common/_transition.module.scss";
+import Router from "next/router";
 
 interface Type {
     children: React.ReactNode,
@@ -11,9 +13,30 @@ interface Type {
 }
 
 export default function layout({ children, bgImg, setbgImg, specificStyles }:Type):JSX.Element{
+    const [displayChildren, setDisplayChildren] = useState(children);
+    const [transitionStage, setTransitionStage] = useState("fadeOut");
+
+    useEffect(() => {
+        setTransitionStage("fadeIn");
+      }, []);
+
+      useEffect(() => {
+        if (children !== displayChildren) setTransitionStage("fadeOut");
+      }, [children, setDisplayChildren, displayChildren]);
+
     return(
         <>
             <Navigation bgImg={bgImg} />
+            {/* <div 
+                onClick={() => {
+                    if (transitionStage === "fadeOut") {
+                        console.log("fading out");
+                        setDisplayChildren(children);
+                        setTransitionStage("fadeIn");
+                    }
+        }} className={`${styles.content} ${styles[transitionStage]}`}>
+                {displayChildren}
+            </div> */}
                 {children}
             <Footer bgImg={bgImg} specificStyles={specificStyles} />
         </>
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,12 @@ import '../styles/globals.css'
 import '../styles/scss/_globals.scss'
 import '../styles/scss/_index.scss'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <Component {...pageProps} />  
+  )
+  
 }
 
 export default MyApp
