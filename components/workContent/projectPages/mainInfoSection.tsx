14382b2240750113932b64f89ea471200f63b601
Iambizi <amirbzmn@gmail.com>
2021-06-21 22:34:20 -0400

    projectPages: saves work on styles for bigger devices

--- components/workContent/projectPages/mainInfoSection.tsx
+++ components/workContent/projectPages/mainInfoSection.tsx
@@ -1,17 +1,14 @@
 import Image from 'next/image';
 import styles from "../../../styles/scss/projectPages/_projectPages.module.scss";
+
 interface Type{
     projects: any;
 }
+
 export default function mainInfoSection( {projects}: Type ):JSX.Element{
     const overlayPlay = ()=>{
 
         const overlay = document.getElementsByClassName('overlay');
-
-        console.log("yoo");
-        console.log(overlay);
-
-        // overlay.classList.add(`${styles.hideOverlay}`);
 
         for(var i = 0; i < overlay.length; i++) {
             ((index)=> {
@@ -22,16 +19,18 @@ export default function mainInfoSection( {projects}: Type ):JSX.Element{
     return(
         <>
             <section className={styles.projectPageSection}>
-                <h3 className={styles.projectTitle}>
-                    {projects.title}
-                </h3>
                 <div className={styles.projectDetailsWrapper}>
-                    <p className={styles.projectDetails}>
-                        {projects.projectInfo}
-                    </p>
+                    <h3 className={styles.projectTitle}>
+                        {projects.title}
+                    </h3>
+                    <div className={styles.projectCredsWrapper}>
+                        <p className={styles.projectCreds}>
+                            {projects.projectInfo}
+                        </p>
+                    </div>
                 </div>
                 <div className={`${styles.projectVideo}`}>
-                    <div onClick={overlayPlay} className={`${styles.videoOverlay} overlay`} style={{backgroundImage: `url(https://kidstudio.co${projects.videoCover})`}}>
+                    <div onClick={ overlayPlay } className={`${styles.videoOverlay} overlay`} style={{backgroundImage: `url(https://kidstudio.co${projects.videoCover})`}}>
                         <div  className={`${styles.videoOverlay} overlay`} style={{backgroundImage: `url(https://kidstudio.co/assets/images/play.png)`}}>
                             {/* <Image
                             unoptimized
