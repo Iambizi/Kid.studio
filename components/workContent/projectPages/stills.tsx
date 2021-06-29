162d19f4443af9e0e3af0d8dcbef5c51ec5d2818
Iambizi <amirbzmn@gmail.com>
2021-06-29 00:44:07 -0400

    projectPages: adds dynamic marginLeft positions

--- components/workContent/projectPages/stills.tsx
+++ components/workContent/projectPages/stills.tsx
@@ -11,43 +11,36 @@ export default function stills( {projects}: Type ):JSX.Element{
     useEffect(()=>{
         const screenWidth = window.innerWidth;
         
-        const stills = [document.getElementsByClassName('stills')];
-
-        const positions = [
-                [ 30, 10, 30, 10, 30, 10, 50, 30, 10, 30 ],
-            
-                [ 50, 10, 30, 50, 30, 10, 30, 10, 30, 10 ],
-
-                [ 50, 10, 30, 50, 10, 50, 10, 50, 10, 30 ] 
-        ];
+        // Fisher-Yates (aka Knuth) Shuffle
+        // function that shuffles values in positions array
+        
+        const shuffle = (array)=>{
+            var currentIndex = array.length,  randomIndex;
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+              // Pick a remaining element...
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex--;
+              // And swap it with the current element.
+              [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+            }
+            return array;
+        }
 
         const position = [
-            30, 10, 30, 10, 30, 10, 50, 30, 10, 30 
-    ];
+            30, 50, 30, 10, 30, 10, 50, 30, 10, 30 
+        ];
+        shuffle(position);
 
         if(screenWidth >= 1200){
             console.log("big screen");
             const stills = Array.from(document.getElementsByClassName("stills") as HTMLCollectionOf<HTMLElement>);
-            // console.log(stills[0].style.marginLeft="50%");
-            console.log(position[i]);
+
             for(var i = 0; i < stills.length; i++) {
                 stills[i].style.marginLeft = `${position[i]}%`;
             }
-        }else{
-            console.log("small screen");
         }
-        console.log(positions[0]);
-        console.log(positions);
-        // for(var i = 0; i < positions.length; i++) {
-        //     ((index)=> {
-        //     //   stills[index].style.left = positions[0];
-        //     values.map((item, i)=>(
-        //         console.log(`${values[i]}:${positions[i]}`)
-        //     ));
-        //     // console.log(positions[i]);
-        //     // console.log(`${values[i]}:${positions[i]}`);
-        //     })(i);
-        //   }
     },[]);
     return(
         <>
