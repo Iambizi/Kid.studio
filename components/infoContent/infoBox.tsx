be997373121fbec8379c045db6c68b411c322fcf
Iambizi <amirbzmn@gmail.com>
2021-07-01 23:07:24 -0400

    infoPages: adds missing files

---
+++ components/infoContent/infoBox.tsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/scss/info/_info.module.scss';
+
+export default function infoBox(){
+    return(
+    <>
+        <div className={styles.infoContainer}>
+                <p>Inspired ny our youth, influenced by our city.</p>
+                <p>→ Creative Direction + Music Videos + Commercials</p>
+                <p>email@kidstudio.co</p>
+            </div>
+    </>
+    )
+}
