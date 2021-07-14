68691930ce85ebec312d0ab1d2a8c1311854510d
Iambizi <amirbzmn@gmail.com>
2021-07-13 21:29:16 -0400

    All pages: adds page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import '../styles/scss/_globals.scss'
 import '../styles/scss/_index.scss'
+import styles from "../styles/scss/common/_transition.module.scss";
+import { PageTransition } from 'next-page-transitions';
 
 
 function MyApp({ Component, pageProps }) {
   return(
-      <Component {...pageProps} />  
+    <PageTransition timeout={500} classNames="page-transition">
+      <Component {...pageProps} />
+  </PageTransition>  
   )
   
 }
