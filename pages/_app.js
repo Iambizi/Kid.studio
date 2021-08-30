6bb431d3c6e941bd29304a0a1e16225e45b85ea1
Iambizi <amirbzmn@gmail.com>
2021-08-29 20:06:10 -0400

    Removes unused base styles

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,6 @@ import '../styles/scss/_globals.scss'
 import '../styles/scss/_index.scss'
 import { useRouter } from "next/router";
 import { PageTransition } from 'next-page-transitions';
-import Router from "next/router";
-
-// Add that code to _app.tsx / _app.jsx
-    const routeChange = () => {
-      // Temporary fix to avoid flash of unstyled content
-      // during route transitions. Keep an eye on this
-      // issue and remove this code when resolved:
-      // https://github.com/vercel/next.js/issues/17464
-
-      const tempFix = () => {
-        const allStyleElems = document.querySelectorAll('style[media="x"]');
-        allStyleElems.forEach((elem) => {
-          elem.removeAttribute("media");
-        });
-      };
-      tempFix();
-    };
-
-   Router.events.on("routeChangeComplete", routeChange );
-   Router.events.on("routeChangeStart", routeChange );
-
 
 function MyApp({ Component, pageProps}) {
   const router = useRouter();
