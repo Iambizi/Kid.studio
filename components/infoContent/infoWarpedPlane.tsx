fce252b0c5c559b0f8e5ef731de33fb91b2039cb
Iambizi <amirbzmn@gmail.com>
2021-10-04 20:55:18 -0400

    InfoPage: cleans up warped image code

--- components/infoContent/infoWarpedPlane.tsx
+++ components/infoContent/infoWarpedPlane.tsx
@@ -44,7 +44,7 @@ export default function inforWarpImg({src}: Type):JSX.Element{
         const mesh = new THREE.Mesh( geometry, material );
         scene.add( mesh );
 
-        screenWidth <= 500 ? mesh.position.y = 4: null;
+        screenWidth <= 500 ? mesh.position.y = 1: null;
 
         const sizes = {
             width: window.innerWidth,
