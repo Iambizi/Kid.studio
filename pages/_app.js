fcccc05bcdf8c3f3f357a7a3388fb387a3935fba
Iambizi <amirbzmn@gmail.com>
2021-10-23 15:35:03 -0400

    cleans up app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,10 @@ import '../styles/scss/_globals.scss'
 import '../styles/scss/_index.scss'
 import { PageTransition } from 'next-page-transitions';
 import React from "react";
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps}) {
+  const router = useRouter();
 
   return(
     <PageTransition timeout={700} classNames="page-transition">
