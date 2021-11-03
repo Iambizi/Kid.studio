77ed6daf4ecfcf1827a786b2fb487e2800b3fca7
Iambizi <amirbzmn@gmail.com>
2021-11-02 23:57:05 -0400

    Adds functionality that determines fixed positioning across all pages

--- components/common/header/navigation.tsx
+++ components/common/header/navigation.tsx
@@ -11,45 +11,42 @@ interface Type {
 
 export default function navigation( { bgImg }:Type ):JSX.Element{
     const router = useRouter();
+
+    //Handles logic for removal of fixed positioning along pages
     const [notFixed, setNotFixed] = useState(false);
-    const path = router.pathname;
-    const workPath = /\bwork\b/;
+    const workPath = /\/work$/gm;
+    const infoPath = /\/info$/gm;
+    const homePath = /\/$/gm;
 
     const setFixedNav = () =>{
-        if(router.pathname.match(workPath)){
+        if(router.pathname.match(workPath) || router.pathname.match(infoPath) || router.pathname.match(homePath) ){
             setNotFixed(true);
             console.log("not fixed");
-        }else{
-            setNotFixed(false);
-            console.log("fixed");
         }
     }
-
 
     useEffect(()=>{
         setFixedNav();
 
-        // router.events.on('beforeHistoryChange', setFixedNav);
-        // return () => {
-        //   router.events.off('beforeHistoryChange', setFixedNav);
-        // };
+        router.events.on('beforeHistoryChange', setFixedNav);
+        return () => {
+          router.events.off('beforeHistoryChange', setFixedNav);
+        };
     },[]);
-
-    console.log(notFixed);
 
     return(
         <>
             <header>
-                <nav className={notFixed ? `${styles.navigation} fixed`: `${styles.navigation}`}>
-                        <Logo bgImg={bgImg} />
-                            <ul className={styles.navLinks}>
-                                <Link href={"/work"}>
-                                    <a className={ bgImg ? `${styles.navLink} ${styles.hoverColor}` : `${styles.navLink}` }>WORK</a>
-                                </Link>
-                                <Link href={"/info"}>
-                                    <a className={ bgImg ? `${styles.navLink} ${styles.hoverColor}` : `${styles.navLink}` }>INFO</a>
-                                </Link>
-                            </ul>
+                <nav className={notFixed ? `${styles.navigation}`: `${styles.navigation} ${styles.fixed}`}>
+                    <Logo bgImg={bgImg} />
+                        <ul className={styles.navLinks}>
+                            <Link href={"/work"}>
+                                <a className={ bgImg ? `${styles.navLink} ${styles.hoverColor}` : `${styles.navLink}` }>WORK</a>
+                            </Link>
+                            <Link href={"/info"}>
+                                <a className={ bgImg ? `${styles.navLink} ${styles.hoverColor}` : `${styles.navLink}` }>INFO</a>
+                            </Link>
+                        </ul>
                 </nav>
             </header>
         </>
--- pages/work/[project].tsx
+++ pages/work/[project].tsx
@@ -5,7 +5,7 @@ import Layout from '../../components/layout';
 import Meta  from '../../components/common/meta';
 import MainInfo from '../../components/workContent/projectPages/mainInfoSection';
 import Stills from '../../components/workContent/projectPages/stills';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/router';
 import styles from '../../styles/scss/common/_footer.module.scss';
 import { connectClient } from '../../components/common/utils/createClient';
@@ -18,6 +18,7 @@ interface Type{
 
 export default function projectPages( { projectPage }: Type):JSX.Element{
     const router = useRouter();
+    const [notFixed, setNotFixed] = useState(false);
 
     async function fetcher(url){
         const res = await fetch(url);
@@ -45,6 +46,7 @@ export default function projectPages( { projectPage }: Type):JSX.Element{
         const removePageScroll = () =>{
               bg.classList.remove("needsScroll");
               console.log('no scrolly');
+              setNotFixed(true);
         }
     
         router.events.on('beforeHistoryChange', removePageScroll);
