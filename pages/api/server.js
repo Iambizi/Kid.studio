13dbae38376173b4504843574aafc80be2dc98c5
Iambizi <amirbzmn@gmail.com>
2021-11-13 07:58:09 -0500

    re-adds react three fiber plane for info page

---
+++ pages/api/server.js
@@ -0,0 +1,11 @@
+// server.js
+const next = require('next')
+const routes = require('./routes')
+const app = next({dev: process.env.NODE_ENV !== 'production'})
+const handler = routes.getRequestHandler(app)
+​
+// With express
+const express = require('express')
+app.prepare().then(() => {
+  express().use(handler).listen(3000)
+})
