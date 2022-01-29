8b6ae35abfa88729fcbd4fab0de86a5d023b05c9
Iambizi <amirbzmn@gmail.com>
2022-01-28 22:56:18 -0500

    Saves progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,61 +2,60 @@ import fs from 'fs';
 import path from 'path';
 import { GetStaticProps } from 'next';
 import Layout from '../components/layout';
-import Meta  from '../components/common/meta';
+import Meta from '../components/common/meta';
 import Content from '../components/homeContent/content';
 import React from "react";
 import { connectClient } from '../components/common/utils/createClient';
 import useSWR from 'swr';
 
-interface Type{
+interface Type {
   homeProjects: any;
   projects: string;
 }
 
-export default function Home({homeProjects, projects}: Type):JSX.Element {
+export default function Home({ homeProjects, projects }: Type): JSX.Element {
   // removes needsScroll class set in project pages from vertical scroll
   // projectPage useEffect hook needs refactoring to avoid calling it again here.
 
-    async function fetcher(url){
-      const res = await fetch(url);
-      return res.json();
-    }
+  async function fetcher(url) {
+    const res = await fetch(url);
+    return res.json();
+  }
 
-    //use swr cache revalidation magic
-    const baseUrl = `https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_ID}/environments/master/entries?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESSKEY}`;
-    const { data } = useSWR(baseUrl, fetcher, { initialData: projects });
-    
+  //use swr cache revalidation magic
+  const baseUrl = `https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_ID}/environments/master/entries?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESSKEY}`;
+  const { data } = useSWR(baseUrl, fetcher, { initialData: projects });
+
 
   return (
     <>
-        <Meta page={"Home"} />
-          <Layout>
-            <Content homeProjects={ homeProjects } projects={ projects } />
-        </Layout>
+      <Meta page={"Home"} />
+      <Layout>
+        <Content homeProjects={homeProjects} projects={projects} />
+      </Layout>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () =>{
+export const getStaticProps: GetStaticProps = async () => {
 
   const res = await connectClient.getEntries({ content_type: 'homePage' });
 
-  const fileToRead = path.join(process.cwd(),'./backEndDummyData/homeProjects.json');
+  const fileToRead = path.join(process.cwd(), './backEndDummyData/homeProjects.json');
   const data = JSON.parse(await fs.readFileSync(fileToRead).toString());
   // const project = data.projects.find(project => project.path === projectPath)
-  const HomeProjects = data.homeProjects.map((item: any, i: number)=>(data.homeProjects[i]))
+  const HomeProjects = data.homeProjects.map((item: any, i: number) => (data.homeProjects[i]))
   // console.log(data.projects[0].path)
-  
-    if (!res) {
-      return {
-          notFound: true
-      };
+  if (!res) {
+    return {
+      notFound: true
+    };
   }
   return {
-      props: {
-          homeProjects: HomeProjects,
-          projects: res.items
-      },
-      revalidate: 300
+    props: {
+      homeProjects: HomeProjects,
+      projects: res.items
+    },
+    revalidate: 300
   }
 }
