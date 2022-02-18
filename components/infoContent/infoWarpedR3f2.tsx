bbd2eea9d5824f920a29bccf95503315718f6ce9
Iambizi <amirbzmn@gmail.com>
2022-02-17 21:34:55 -0500

    cleans up code

--- components/infoContent/infoWarpedR3f2.tsx
+++ components/infoContent/infoWarpedR3f2.tsx
@@ -5,14 +5,14 @@ import styles from "../../styles/scss/homePage/_carousel.module.scss";
 import { isMobile } from 'react-device-detect';
 
 
-interface Type{
+interface Type {
     src: string;
 }
 
-export default function WarpedImage({ src }:Type):JSX.Element{
+export default function WarpedImage({ src }: Type): JSX.Element {
 
-    const InfoPlane = (props: any) =>{
-        
+    const InfoPlane = (props: any) => {
+
         const infoPlaneRef = useRef<THREE.Mesh>();
 
         let hover_dist = 0.3;
@@ -23,40 +23,39 @@ export default function WarpedImage({ src }:Type):JSX.Element{
         let mouse = { x: 0, y: 0 };
 
         const loader = new THREE.TextureLoader();
-        
+
         const texture1 = loader.load(`${src}`);
 
         const width = isMobile ? 3.1 : 9;
         const height = isMobile ? 1.7 : 5;
-            
+
         useFrame((state, delta) => {
             animateMesh(state);
         });
 
-        const animateMesh = (state)=> {
-                    
-                const hoverMove = () => {
-                        mouse.x > 0.5 ? infoPlaneRef.current.rotation.y < hover_dist && (infoPlaneRef.current.rotation.y += 0.002) : mouse.x < 0.5 && infoPlaneRef.current.rotation.y > -hover_dist && (infoPlaneRef.current.rotation.y -= 0.002),
-                        mouse.y > 0.5 ? infoPlaneRef.current.rotation.x < hover_dist && (infoPlaneRef.current.rotation.x += 0.002) : mouse.y < 0.5 && infoPlaneRef.current.rotation.x > -hover_dist && (infoPlaneRef.current.rotation.x -= 0.002);
-                    (infoPlaneRef.current.rotation.y > hover_dist || infoPlaneRef.current.rotation.y < -hover_dist) && (infoPlaneRef.current.rotation.x > hover_dist || infoPlaneRef.current.rotation.x < -hover_dist) && (hovering = true);
-                }
-                const snapBack = () => {
-                    let speed = 0.005;
-                    if (infoPlaneRef.current.rotation.x < 0) infoPlaneRef.current.rotation.x += speed;
-                    if (infoPlaneRef.current.rotation.x > 0) infoPlaneRef.current.rotation.x -= speed;
-                    if (infoPlaneRef.current.rotation.y < 0) infoPlaneRef.current.rotation.y += speed;
-                    if (infoPlaneRef.current.rotation.y > 0) infoPlaneRef.current.rotation.y -= speed;
-                }
-                const hover = () => {
-                    i == timerx && (i = 0);
-                    timerx / 2 > i ? ((infoPlaneRef.current.rotation.x += 3e-4), (infoPlaneRef.current.rotation.y -= 3e-4)) : ((infoPlaneRef.current.rotation.x -= 3e-4), (infoPlaneRef.current.rotation.y += 3e-4));
-                    i++;
-                }
-                //  hovering ? hover() : hoverMove();
-                 snapping ? snapBack() : hovering ? hover() : hoverMove();                
+        const animateMesh = (state) => {
+
+            const hoverMove = () => {
+                mouse.x > 0.5 ? infoPlaneRef.current.rotation.y < hover_dist && (infoPlaneRef.current.rotation.y += 0.002) : mouse.x < 0.5 && infoPlaneRef.current.rotation.y > -hover_dist && (infoPlaneRef.current.rotation.y -= 0.002),
+                    mouse.y > 0.5 ? infoPlaneRef.current.rotation.x < hover_dist && (infoPlaneRef.current.rotation.x += 0.002) : mouse.y < 0.5 && infoPlaneRef.current.rotation.x > -hover_dist && (infoPlaneRef.current.rotation.x -= 0.002);
+                (infoPlaneRef.current.rotation.y > hover_dist || infoPlaneRef.current.rotation.y < -hover_dist) && (infoPlaneRef.current.rotation.x > hover_dist || infoPlaneRef.current.rotation.x < -hover_dist) && (hovering = true);
+            }
+            const snapBack = () => {
+                let speed = 0.005;
+                if (infoPlaneRef.current.rotation.x < 0) infoPlaneRef.current.rotation.x += speed;
+                if (infoPlaneRef.current.rotation.x > 0) infoPlaneRef.current.rotation.x -= speed;
+                if (infoPlaneRef.current.rotation.y < 0) infoPlaneRef.current.rotation.y += speed;
+                if (infoPlaneRef.current.rotation.y > 0) infoPlaneRef.current.rotation.y -= speed;
+            }
+            const hover = () => {
+                i == timerx && (i = 0);
+                timerx / 2 > i ? ((infoPlaneRef.current.rotation.x += 3e-4), (infoPlaneRef.current.rotation.y -= 3e-4)) : ((infoPlaneRef.current.rotation.x -= 3e-4), (infoPlaneRef.current.rotation.y += 3e-4));
+                i++;
+            }
+            snapping ? snapBack() : hovering ? hover() : hoverMove();
         }
 
-        useEffect(()=>{
+        useEffect(() => {
 
             const onMouseDown = (e) => {
                 snapping = true;
@@ -79,24 +78,24 @@ export default function WarpedImage({ src }:Type):JSX.Element{
             return () => {
                 document.removeEventListener("mousemove", onDocumentMouseMove, false);
                 document.removeEventListener("mousedown", onMouseDown, false);
-                document.removeEventListener("mouseup", onMouseUp, false); 
+                document.removeEventListener("mouseup", onMouseUp, false);
             };
         })
 
-            return(
-                <mesh {...props} ref={infoPlaneRef}>
-                    <planeGeometry args={[width, height]} />
-                    <meshBasicMaterial  map={texture1} />
-                </mesh>
-            )
+        return (
+            <mesh {...props} ref={infoPlaneRef}>
+                <planeGeometry args={[width, height]} />
+                <meshBasicMaterial map={texture1} />
+            </mesh>
+        )
     }
 
-    return(
+    return (
         <>
             <div className={`${styles.homeScene} homeScene`}>
                 <Canvas dpr={[1, 2]}>
-                   { isMobile ? <InfoPlane position={[0, -.7, 0]} /> : <InfoPlane position={[0, 0, 0]} /> }
-                   {/* <InfoPlane position={[0, 0, 0]} /> */}
+                    {isMobile ? <InfoPlane position={[0, -.7, 0]} /> : <InfoPlane position={[0, 0, 0]} />}
+                    {/* <InfoPlane position={[0, 0, 0]} /> */}
                 </Canvas>
             </div>
         </>
