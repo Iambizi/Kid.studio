cedc808e5a9b8989740db25f1277fa00e6f7b607
Iambizi <amirbzmn@gmail.com>
2022-03-04 08:29:26 -0500

    attempting to fix mobile canvas overflow

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/scss/_index.scss';
 import { useRouter } from 'next/router';
 import { PageTransition } from 'next-page-transitions';
 import React, { useEffect } from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -29,9 +30,13 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <PageTransition timeout={700} classNames="page-transition">
-      <Component {...pageProps} key={router.route} />
-    </PageTransition>
+    <><Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+    </Head>
+      <PageTransition timeout={700} classNames="page-transition">
+        <Component {...pageProps} key={router.route} />
+      </PageTransition>
+    </>
   );
 }
 
