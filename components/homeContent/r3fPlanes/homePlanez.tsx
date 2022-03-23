6e2a9fbd07bc44391cf7c791664654ca6d52138a
Iambizi <amirbzmn@gmail.com>
2022-03-22 22:21:26 -0400

    Adds drag move function

--- components/homeContent/r3fPlanes/homePlanez.tsx
+++ components/homeContent/r3fPlanes/homePlanez.tsx
@@ -18,14 +18,15 @@ export const  HomePlanez = ( { projects, position, projectIndex, ...props}: Type
     let hovering = false;
     let snapping = false;
     let mouseDown = false;
-    let prevMouse = { x: 0, y: 0 };
-    let snapback = { x: 0, y: 0 };
     let hover_dist = 0.3;
     let i = 0;
     let timerx = 500;
     let mouse = { x: 0, y: 0 };
+    let distMouse = { x: 0, y: 0 };
+    let prevMouse = { x: 0, y: 0 };
+    let snapback = { x: 0, y: 0 };
 
-    const homePlaneRef1 = useRef<THREE.Mesh>();
+    const homePlaneRef = useRef<THREE.Mesh>();
 
     const textures = useTexture(src);
 
@@ -37,25 +38,32 @@ export const  HomePlanez = ( { projects, position, projectIndex, ...props}: Type
     const animateMesh = (state) => {
 
         const hoverMove = () => {
-            mouse.x > 0.5 ? homePlaneRef1.current.rotation.y < hover_dist && (homePlaneRef1.current.rotation.y += 0.002) : mouse.x < 0.5 && homePlaneRef1.current.rotation.y > -hover_dist && (homePlaneRef1.current.rotation.y -= 0.002),
-                mouse.y > 0.5 ? homePlaneRef1.current.rotation.x < hover_dist && (homePlaneRef1.current.rotation.x += 0.002) : mouse.y < 0.5 && homePlaneRef1.current.rotation.x > -hover_dist && (homePlaneRef1.current.rotation.x -= 0.002);
-            (homePlaneRef1.current.rotation.y > hover_dist || homePlaneRef1.current.rotation.y < -hover_dist) && (homePlaneRef1.current.rotation.x > hover_dist || homePlaneRef1.current.rotation.x < -hover_dist) && (hovering = true);
+            mouse.x > 0.5 ? homePlaneRef.current.rotation.y < hover_dist && (homePlaneRef.current.rotation.y += 0.002) : mouse.x < 0.5 && homePlaneRef.current.rotation.y > -hover_dist && (homePlaneRef.current.rotation.y -= 0.002),
+                mouse.y > 0.5 ? homePlaneRef.current.rotation.x < hover_dist && (homePlaneRef.current.rotation.x += 0.002) : mouse.y < 0.5 && homePlaneRef.current.rotation.x > -hover_dist && (homePlaneRef.current.rotation.x -= 0.002);
+            (homePlaneRef.current.rotation.y > hover_dist || homePlaneRef.current.rotation.y < -hover_dist) && (homePlaneRef.current.rotation.x > hover_dist || homePlaneRef.current.rotation.x < -hover_dist) && (hovering = true);
         }
 
         
         const hover = () => {
             i === timerx && (i = 0);
-            timerx / 2 > i ? ((homePlaneRef1.current.rotation.x += 3e-4), (homePlaneRef1.current.rotation.y -= 3e-4)) : ((homePlaneRef1.current.rotation.x -= 3e-4), (homePlaneRef1.current.rotation.y += 3e-4));
+            timerx / 2 > i ? ((homePlaneRef.current.rotation.x += 3e-4), (homePlaneRef.current.rotation.y -= 3e-4)) : ((homePlaneRef.current.rotation.x -= 3e-4), (homePlaneRef.current.rotation.y += 3e-4));
             i++;
         }
         
         const snapBack = () => {
-            homePlaneRef1.current.rotation.x < 0.002 && homePlaneRef1.current.rotation.x > -0.002 && homePlaneRef1.current.rotation.y < 0.002 && homePlaneRef1.current.rotation.y > -0.002 && (snapping = false);
-            homePlaneRef1.current.rotation.x -= snapback.x; 
-            homePlaneRef1.current.rotation.y -= snapback.y;
+            homePlaneRef.current.rotation.x < 0.002 && homePlaneRef.current.rotation.x > -0.002 && homePlaneRef.current.rotation.y < 0.002 && homePlaneRef.current.rotation.y > -0.002 && (snapping = false);
+            homePlaneRef.current.rotation.x -= snapback.x; 
+            homePlaneRef.current.rotation.y -= snapback.y;
         }
-        
-        snapping ? snapBack() :hovering ? hover() : hoverMove();
+
+        const dragMove = () => {
+            distMouse.x = prevMouse.x - mouse.x;
+            distMouse.y = prevMouse.y - mouse.y;
+            homePlaneRef.current.rotation.y -= 2 * distMouse.x; 
+            homePlaneRef.current.rotation.x -= 2 * distMouse.y;
+        }
+
+        mouseDown ? dragMove() : snapping ? snapBack() : hovering ? hover() : hoverMove();
     }
 
     useFrame((state) => {
@@ -76,8 +84,8 @@ export const  HomePlanez = ( { projects, position, projectIndex, ...props}: Type
         const onMouseUp = (e) => {
             mouseDown = false; 
             snapping = true; 
-            snapback.x = homePlaneRef1.current.rotation.x / 60; 
-            snapback.y = homePlaneRef1.current.rotation.y / 60;
+            snapback.x = homePlaneRef.current.rotation.x / 60; 
+            snapback.y = homePlaneRef.current.rotation.y / 60;
             console.log("im cliiiiiicked up");
         }
 
@@ -101,7 +109,7 @@ export const  HomePlanez = ( { projects, position, projectIndex, ...props}: Type
 
     return (
         <>
-            <mesh {...props} ref={homePlaneRef1} position={[position, 0, .1]}>
+            <mesh {...props} ref={homePlaneRef} position={[position, 0, .1]}>
                 <planeBufferGeometry args={[width, height]} />
                 <meshBasicMaterial map={textures} />
             </mesh>
