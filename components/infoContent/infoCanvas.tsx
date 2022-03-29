0efc53abaa13e8603b40ef5b72030032665e7c41
Iambizi <amirbzmn@gmail.com>
2022-03-29 17:53:53 -0400

    Adds higher plane def for mobile

--- components/infoContent/infoCanvas.tsx
+++ components/infoContent/infoCanvas.tsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import styles from "../../styles/scss/homePage/_carousel.module.scss";
 import Loader from "../common/R3FLoader";
 import {InfoPlane} from "./infoWarpedR3f3";
+import { isMobile } from 'react-device-detect';
 
 interface Type {
     src: string;
@@ -13,7 +14,7 @@ export const InfoPlaneCanvas = ({ src }: Type): JSX.Element  => {
     return (
         <>
             <div className={`${styles.homeScene}`}>
-                <Canvas dpr={[0, 1]}>
+                <Canvas dpr={isMobile ? [1, 2] : [0, 1]}>
                     <Suspense fallback={<Loader/>}>
                         <InfoPlane src={src} />
                     </Suspense>
