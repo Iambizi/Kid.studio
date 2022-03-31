ea3e46b480cc15f8d8ff829613e60d0bb05b6d99
Iambizi <amirbzmn@gmail.com>
2022-03-30 20:14:43 -0400

    Works on fixing error handling

---
+++ pages/500.tsx
@@ -0,0 +1,19 @@
+import Layout from '../components/layout';
+import Meta from '../components/common/meta';
+import styles from '../styles/scss/common/_error.module.scss';
+
+export default function Custom500() {
+    return (
+        <>
+            <Meta page={"500"} />
+            <Layout>
+                <>
+                    <div className={styles.error}>
+                        <h1>500 NAH B.</h1>
+                        <img src="http://media.giphy.com/media/Kavm9lxU8Ljc4/giphy.gif" />
+                    </div>
+                </>
+            </Layout>
+        </>
+    )
+}
