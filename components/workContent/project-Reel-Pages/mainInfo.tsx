26e5555560d187c9ce8b5fc3a59bb5621d9a8560
Iambizi <amirbzmn@gmail.com>
2022-04-05 21:52:59 -0400

    Foler name changes

---
+++ components/workContent/project-Reel-Pages/mainInfo.tsx
@@ -0,0 +1,81 @@
+import Image from 'next/image';
+import styles from "../../../styles/scss/projectPages/_projectPages.module.scss";
+import React, { useState, useEffect, useRef } from "react";
+import { isMobile } from 'react-device-detect';
+
+interface Type {
+    title: any;
+    details: any;
+    videoCover: any;
+    playButton: any;
+    projectVideo: any;
+}
+
+const MainInfoSection = ({ title, details, videoCover, playButton, projectVideo }: Type): JSX.Element => {
+    
+    const [overLayClick, setOverLayClick] = useState(false);
+
+    const overlayPlay = () => {
+        setOverLayClick(true);
+        { projectVideo ? setOverLayClick(true) : setOverLayClick(false) }
+    }
+
+    const ref = useRef<HTMLElement | any>(null!);
+
+    // functionality for offset page scroll
+    const titleScroll = () => {
+
+        let pageY = window.pageYOffset;
+        let transY = 0;
+        let diff = 0;
+        
+        if (!isMobile && ref.current) {
+            setTimeout(() => {
+                if (diff = pageY - transY) {
+                    transY += 2 * diff
+                }
+                ref.current.style.transform = `translateY(${pageY * -0.1}px)`;
+
+            }, 300);
+        } else {
+            return null;
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', titleScroll);
+    }, []);
+
+    return (
+        <>
+            <section className={styles.projectPageSection}>
+                <div className={styles.projectDetailsWrapper}>
+                    <h3 className={`${styles.projectTitle} title`} ref={ref}>
+                        {title}
+                    </h3>
+                    <div className={styles.projectCredsWrapper}>
+                        <p className={styles.projectCreds}>
+                            {details}
+                        </p>
+                    </div>
+                </div>
+                <div className={`${styles.projectVideo} video`}>
+                    <div onClick={overlayPlay} className={overLayClick ? `${styles.videoOverlay} ${styles.hideOverlay} overlay` : `${styles.videoOverlay} overlay`} style={{ backgroundImage: `url(${videoCover.fields.file.url})` }}>
+                    <div className={overLayClick ? `${styles.videoOverlay} ${styles.hideOverlay} overlay` : `${styles.videoOverlay} overlay`} style={{ backgroundImage: `url(${playButton})` }}>
+                            <Image
+                                className={styles.videoCover}
+                                src={`https:${videoCover.fields.file.url}`}
+                                alt="Main video/image still"
+                                width={videoCover.fields.file.details.image.width}
+                                height={videoCover.fields.file.details.image.height}
+                            />
+                        </div>
+                    </div>
+                    <iframe className={styles.video} id="vimeo1aolzk8" src={`${projectVideo}`} frameBorder="0" allowFullScreen></iframe>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default MainInfoSection;
