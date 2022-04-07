2f60c3060e7cdb11ae332671913cc3485132b16b
Iambizi <amirbzmn@gmail.com>
2022-04-06 21:56:46 -0400

    Adds common assets: Logos

--- components/common/header/logo.tsx
+++ components/common/header/logo.tsx
@@ -13,7 +13,6 @@ const Logo = ( { bgImg, LogoBlack, LogoWhite }: Type ): JSX.Element => {
             <div className={styles.logo}>
                 <Link href={"/"}>
                     <a>
-                        {/* <img src={ bgImg ? "https://kidstudio.co/assets/images/kidwhite.png" : "https://kidstudio.co/assets/images/kid.png" } alt="Kid Studio logo" width={85} */}
                         <img src={ bgImg ? `https:${LogoWhite}` : `https:${LogoBlack}` } alt="Kid Studio logo" width={85}
                         height={46.3} />
                     </a>
