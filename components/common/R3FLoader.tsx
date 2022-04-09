c26c1b5daf48f5941da2e50062361fb887b7b5f4
Iambizi <amirbzmn@gmail.com>
2022-04-08 23:03:25 -0400

    Uses mp4 for loader

--- components/common/R3FLoader.tsx
+++ components/common/R3FLoader.tsx
@@ -7,12 +7,17 @@ interface Type{
 }
 
 const Loader: React.FC<Type> = ({loaderLink}): JSX.Element => {
+    console.log(loaderLink);
     return (
         <>
             <Html
                 prepend
                 center>
-                <img className={styles.loader} src={`https:${loaderLink}`} alt="loader"/>
+                {/* <img className={styles.loader} src={`https:${loaderLink}`} alt="loader"/> */}
+                <video className={styles.loader} autoPlay loop muted playsInline>
+                    <source src={`https:${loaderLink}`} type="video/webm"/>
+                    <source src={`https:${loaderLink}`} type="video/mp4"/>
+                </video>
             </Html>
         </>
     )
