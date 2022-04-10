cb74c00291855f6fb5ffe874492f3fa5bcdfa6ad
Iambizi <amirbzmn@gmail.com>
2022-04-09 20:57:18 -0400

    Reverts back to using gifs

--- components/common/R3FLoader.tsx
+++ components/common/R3FLoader.tsx
@@ -12,11 +12,11 @@ const Loader: React.FC<Type> = ({loaderLink}): JSX.Element => {
             <Html
                 prepend
                 center>
-                {/* <img className={styles.loader} src={`https:${loaderLink}`} alt="loader"/> */}
-                <video className={styles.loader} autoPlay loop muted playsInline>
+                <img className={styles.loader} src={`https:${loaderLink}`} alt="loader"/>
+                {/* <video className={styles.loader} autoPlay loop muted playsInline>
                     <source src={`https:${loaderLink}`} type="video/webm"/>
                     <source src={`https:${loaderLink}`} type="video/mp4"/>
-                </video>
+                </video> */}
             </Html>
         </>
     )
