4a604df12549c5b423c32970e6fabad75e71261e
Iambizi <amirbzmn@gmail.com>
2022-10-15 23:13:48 -0400

    Saves work

--- pages/api/queries.js
+++ pages/api/queries.js
@@ -1,21 +1,31 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
-export const homePageQuery = gql `
-query {
-    homePageCollection{
-      items{
+export const homePageQuery = gql`
+  query {
+    homePageCollection {
+      items {
         title
         slug
-        featuredProjectImage{
+        featuredProjectImage {
           url
         }
       }
     }
-    commonAssetsCollection{
-        items{
-          loader{
-            url
-          }
+    commonAssetsCollection {
+      items {
+        loader {
+          url
         }
       }
-  }`;
+    }
+  }
+`;
+
+
+
+
+// export const commonAssetsQuery = gql`
+//     query{
+
+//     }
+//   `;
