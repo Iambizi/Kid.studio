b0de86e6c71f1d1371fb4bd469c9ab1661843333
Iambizi <amirbzmn@gmail.com>
2022-10-22 13:47:29 -0400

    work on commonQuery call

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navigation from "../components/common/header/navigation";
 import Footer from "../components/common/footer";
 import Flash from "../components/common/flash";
-import apolloClient from "../pages/api/apollo-client";
-
+import {commonPageTypes} from "../components/props/propTypes";
 
 interface Types {
     children: React.ReactNode,
@@ -11,6 +10,7 @@ interface Types {
     setbgImg?: any;
     specificStyles?: string;
     commonAssets: any;
+    // commonAssets: commonPageTypes;
 }
 
 const Layout:React.FC<Types> = ({ children, bgImg, specificStyles, commonAssets }):JSX.Element =>{
@@ -18,6 +18,27 @@ const Layout:React.FC<Types> = ({ children, bgImg, specificStyles, commonAssets 
     const LogoBlack = commonAssets.siteLogos[0].fields.file.url;
     const LogoWhite = commonAssets.siteLogos[1].fields.file.url;
     const FlashImages = commonAssets.flashAssets;
+
+    const fetchCommonAssest = async () => {
+         fetch('https://rickandmortyapi.com/graphql').then((res) => {
+            if (res.ok) {
+              console.log("success");
+              return res.json();
+            } else {
+              throw res;
+            }
+          })
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+    }
+
+    useEffect(()=>{
+        fetchCommonAssest();
+    },[]);
 
     return(
         <>
